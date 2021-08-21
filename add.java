782c47b08260944c2ad2fe11a5d5a4252712b955
renuka <renukadevi29730@gmail.com>
2021-08-21 08:35:03 -0700

    addition feature : addition is opertation

---
+++ add.java
@@ -0,0 +1,14 @@
+public class Add {
+
+
+        public static int add(int firstNo, int secondNo){
+            return firstNo+secondNo;
+        }
+        public static void main(String args[])
+        {   int firstNo=2,secondNo=3;
+            int sum=add(firstNo,secondNo);
+            System.out.println(sum);
+        }
+    }
+
+
