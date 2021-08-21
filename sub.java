9ed9db0dadcc19c9ac1105970a2e1c47615394de
renuka <renukadevi29730@gmail.com>
2021-08-21 09:14:20 -0700

    subtraction

---
+++ sub.java
@@ -0,0 +1,12 @@
+public class sub {
+
+
+        public static int sub(int firstNo, int secondNo){
+            return firstNo-secondNo;
+        }
+        public static void main(String args[])
+        {   int firstNo=2,secondNo=3;
+            int sum=sub(firstNo,secondNo);
+            System.out.println(sum);
+        }
+    }
